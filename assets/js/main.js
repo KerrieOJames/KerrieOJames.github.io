d1f76dbe2506079a9f573dfde33974903ee7cf3e
KerrieOJames <Bluecheese3>
2017-06-02 13:55:54 +1000

    animate yo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,45 @@
 
+$(document).ready(function(){
+
+
+  $('.contain').mouseenter(function() {
+     $('.sun').addClass('showme');
+   });
+
+
+   $('.contain').mouseleave(function() {
+  $('.sun').removeClass('showme');
+});
+
+
+$('.menu').mouseenter(function() {
+   $('.sun2').addClass('showme');
+ });
+
+
+ $('.menu').mouseleave(function() {
+$('.sun2').removeClass('showme');
+});
+
+$('.menu').mouseenter(function() {
+   $('.sun3').addClass('showme');
+ });
+
+
+ $('.menu').mouseleave(function() {
+$('.sun3').removeClass('showme');
+});
+
+
+$('.menu').mouseenter(function() {
+   $('.sun4').addClass('showme');
+ });
+
+
+ $('.menu').mouseleave(function() {
+$('.sun4').removeClass('showme');
+});
+
+
+
+});
