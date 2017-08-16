a33bf0022877c8b37c429eee96ec775620eca0c7
KerrieOJames <Bluecheese3>
2017-08-16 22:09:37 +1000

    page 4

---
+++ 5posters/assets/js/page1.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+
+
+
+
+
+$('.number').mouseenter(function() {
+   $('.footnote').addClass('showme');
+ });
+
+
+ $('.number').mouseleave(function() {
+  $('.footnote').removeClass('showme');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
---
+++ 5posters/assets/js/page4.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+
+
+
+$('.text').mouseenter(function() {
+   $('.circle').addClass('showme');
+ });
+
+
+
+$('.text').mouseleave(function() {
+  $('.circle').removeClass('showme');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
