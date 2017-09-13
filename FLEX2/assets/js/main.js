d6d50edadf9dbb8eb2b5af8573068ce47099aa41
KerrieOJames <Bluecheese3>
2017-09-13 13:07:54 +1000

    oh dear

--- FLEX2/assets/js/main.js
+++ FLEX2/assets/js/main.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+$('.blue').mouseenter(function() {
+   $('.lights').addClass('showme');
+ });
 
+ $('.blue').mouseleave(function() {
+   $('.lights').removeClass('showme');
+ });
 
 
 
