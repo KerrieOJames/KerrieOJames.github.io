04aabe94e11ce8f7618e2cb987720e2863588965
KerrieOJames <Bluecheese3>
2017-09-14 13:15:17 +1000

    yo

--- archive/assets/js/main.js
+++ archive/assets/js/main.js
@@ -3,13 +3,36 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+$('.hoverlights').mouseenter(function() {
+   $('.lights').addClass('showme');
+ });
+
+ $('.hoverlights').mouseleave(function() {
+   $('.lights').removeClass('showme');
+ });
+
+
+ $('.hoverday').mouseenter(function() {
+    $('.days').addClass('showme');
+  });
+
+  $('.hoverday').mouseleave(function() {
+    $('.days').removeClass('showme');
+  });
 
 
 
+ $(document).ready(function(){
+    $("img").click(function(){
+         $("img").animate({height: "200px"});
+     });
+ });
 
-
-
-
+ $(document).mouseleave(function(){
+    $("img").click(function(){
+         $("img").removeClass({height: "200px"});
+     });
+ });
 
 
 
