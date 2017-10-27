2499295d012756c8f28157f0ff1e7d972f1b6ac8
KerrieOJames <Bluecheese3>
2017-10-27 11:16:57 +1100

    move and copy folders plez work

---
+++ workingfiles/assets/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+
+
+
+
+$('.animation').mouseenter(function() {
+   $('.hovermotion').addClass('showme');
+ });
+
+
+
+$('.animation').mouseleave(function() {
+  $('.hovermotion').removeClass('showme');
+});
+
+$('.ice').mouseenter(function() {
+   $('.icetext').addClass('showme');
+ });
+
+
+
+$('.ice').mouseleave(function() {
+  $('.icetext').removeClass('showme');
+});
+
+
+
+$('.waterbody').mouseenter(function() {
+   $('.watertext').addClass('showme');
+ });
+
+
+
+$('.waterbody').mouseleave(function() {
+  $('.watertext').removeClass('showme');
+});
+
+
+
+
+
+
+
+
+
+
+
+  });
